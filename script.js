e855d6ffd965515c98fe1f2cf7b3dc9426d092a2
Julien Guntermann <julienguntermann@web.de>
2021-12-27 14:16:20 +0100

    Add arrays of adjectives to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var summaryAdjArr = ['rainy', 'wet', 'humid', 'dry', 'arid', 'frigid', 'foggy', 'windy', 'stormy', 'breezy', 'windless', 'calm', 'still']
+
+var dayAdjArr = ['long', 'dark', 'cold', 'black', 'stormy', 'clear', 'wild', 'quiet', 'terrible', 'criminal', 'silent', 'awful']
+
+var nightAdjArr = ['happy', 'peaceful', 'wonderful', 'inspirational', 'bright', 'energetic', 'lovely', 'glorious', 'fabulous', 'grand']
+
+var driveAdjArr = ['dangerously', 'aggressively', 'carefully', 'drunk', 'fast', 'erraticaly', 'poorly', 'dominantly', 'recklessly', 'reasonably']
+
+
+
+// function randomWeatherForecast() {
+
+//     return `Tomorrow, the weather will be ${summaryAdj}! While the night is ${nightAdj}, the day will be ${dayAdj}. So please drive ${driveAdj}!`
+
+// }
+
+// randomWeatherForecast()
+
